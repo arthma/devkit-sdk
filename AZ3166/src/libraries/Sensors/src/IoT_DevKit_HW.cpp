3264e9153b6a3b28567b68b43ad7a2864ec81cf3
Arthur Ma <arthma@microsoft.com>
2019-01-18 01:50:53 +0800

    Update HW C APIs.

--- AZ3166/src/libraries/Sensors/src/IoT_DevKit_HW.cpp
+++ AZ3166/src/libraries/Sensors/src/IoT_DevKit_HW.cpp
@@ -21,10 +21,15 @@ static char connString[AZ_IOT_HUB_MAX_LEN + 1] = {'\0'};
 static const char *boardName = NULL;
 static const char *boardInfo = NULL;
 
-static volatile uint64_t blinkTimeStart = 0;
-static volatile int64_t blinkTime = -1;
-static int ledStat = 0;
-static int ledColor = 0;
+// Blink the RGB LED
+static volatile uint64_t blinkRGBLEDTimeStart = 0;
+static volatile int64_t blinkRGBLEDTime = -1;
+static volatile int colorRGBLED = 0;
+
+// Blink the User LED
+static volatile uint64_t blinkUserLEDTimeStart = 0;
+static volatile int64_t blinkUserLEDTime = -1;
+static volatile int userLEDStat = 0;
 
 static struct _tagRGB
 {
@@ -42,21 +47,25 @@ static bool initWiFi(void)
 {
     if (WiFi.begin() == WL_CONNECTED)
     {
-        return true;
+        return 0;
     }
     else
     {
         LogError("Failed to initialize Wi-Fi.");
-        return false;
+        return -100;
     }
 }
 
-int initIoTDevKit(void)
+int initIoTDevKit(int isShowInfo)
 {
     // Init the screen
     Screen.init();
-    Screen.print(0, "IoT DevKit");
-    Screen.print(2, "Initializing...");
+
+    if (isShowInfo)
+    {
+        Screen.print(0, "IoT DevKit");
+        Screen.print(2, "Initializing...");
+    }
 
     // Serial
     Serial.begin(115200);
@@ -65,60 +74,91 @@ int initIoTDevKit(void)
     pinMode(LED_WIFI, OUTPUT);
     pinMode(LED_AZURE, OUTPUT);
     pinMode(LED_USER, OUTPUT);
+    pinMode(USER_BUTTON_A, INPUT);
+    pinMode(USER_BUTTON_B, INPUT);
 
     // Turn off the RGB Led
     rgbLed.turnOff();
 
     // Init I2C bus
+    if (isShowInfo)
+    {
+        Screen.print(3, "  I2C");
+    }
     if ((ext_i2c = new DevI2C(D14, D15)) == NULL)
     {
         LogError("Failed to initialize I2C.");
-        return false;
+        return -101;
     }
-
+    
     // Init the gyroscope and accelerator sensor
+    if (isShowInfo)
+    {
+        Screen.print(3, "  LSM6DSL");
+    }
     if ((acc_gyro = new LSM6DSLSensor(*ext_i2c, D4, D5)) == NULL)
     {
         LogError("Failed to initialize gyroscope and accelerator sensor.");
-        return false;
+        return -102;
     }
     acc_gyro->init(NULL);
     acc_gyro->enableAccelerator();
     acc_gyro->enableGyroscope();
 
     // Init the humidity and temperature sensor
+    if (isShowInfo)
+    {
+        Screen.print(3, "  HTS221");
+    }
     if ((ht_sensor = new HTS221Sensor(*ext_i2c)) == NULL)
     {
         LogError("Failed to initialize humidity and temperature sensor.");
-        return false;
+        return -103;
     }
     ht_sensor->init(NULL);
+    ht_sensor->reset();
 
     // Init the magnetometer sensor
+    if (isShowInfo)
+    {
+        Screen.print(3, "  LIS2MDL");
+    }
     if ((magnetometer = new LIS2MDLSensor(*ext_i2c)) == NULL)
     {
         LogError("Failed to initialize magnetometer sensor.");
-        return false;
+        return -104;
     }
     magnetometer->init(NULL);
 
-    // Init Irda
+    // Init IrDA
+    if (isShowInfo)
+    {
+        Screen.print(3, "  IrDA");
+    }
     if ((IrdaSensor = new IRDASensor()) == NULL)
     {
         LogError("Failed to initialize IrDa sensor.");
-        return false;
+        return -105;
     }
     IrdaSensor->init();
 
     // Init pressure sensor
+    if (isShowInfo)
+    {
+        Screen.print(3, "  LPS22HB");
+    }
     if ((pressureSensor = new LPS22HBSensor(*ext_i2c)) == NULL)
     {
         LogError("Failed to initialize pressure sensor.");
-        return false;
+        return -106;
     }
     pressureSensor->init(NULL);
 
     // Init WiFi
+    if (isShowInfo)
+    {
+        Screen.print(3, "  Wi-Fi");
+    }
     return initWiFi();
 }
 
@@ -157,7 +197,6 @@ float getDevKitHumidityValue(void)
 {
     float humidity = 0;
     ht_sensor->getHumidity(&humidity);
-    LogInfo(">>Humidity %f", humidity);
     return humidity;
 }
 
@@ -170,15 +209,13 @@ float getDevKitTemperatureValue(int isFahrenheit)
         //convert from C to F
         temperature = temperature * 1.8 + 32;
     }
-    LogInfo(">>Temperature %f", temperature);
     return temperature;
 }
 
 float getDevKitPressureValue(void)
 {
     float pressure = 0;
-    pressureSensor->getTemperature(&pressure);
-    LogInfo(">>Pressure %f", pressure);
+    pressureSensor->getPressure(&pressure);
     return pressure;
 }
 
@@ -189,7 +226,6 @@ void getDevKitMagnetometerValue(int *x, int *y, int *z)
     *x = axes[0];
     *y = axes[1];
     *z = axes[2];
-    LogInfo(">>Magnetometer %d, %d, %d", *x, *y, *z);
 }
 
 void getDevKitGyroscopeValue(int *x, int *y, int *z)
@@ -199,7 +235,6 @@ void getDevKitGyroscopeValue(int *x, int *y, int *z)
     *x = axes[0];
     *y = axes[1];
     *z = axes[2];
-    LogInfo(">>Gyroscope %d, %d, %d", *x, *y, *z);
 }
 
 void getDevKitAcceleratorValue(int *x, int *y, int *z)
@@ -209,45 +244,133 @@ void getDevKitAcceleratorValue(int *x, int *y, int *z)
     *x = axes[0];
     *y = axes[1];
     *z = axes[2];
-    LogInfo(">>Accelerator %d, %d, %d", *x, *y, *z);
 }
 
-void blinkDevKitLED(int ms)
+void turnOnUserLED(void)
 {
-    blinkTimeStart = SystemTickCounterRead();
-    blinkTime = ms;
-    ledStat = 0;
-    ledColor = 0;
-    digitalWrite(LED_USER, ledStat);
+    digitalWrite(LED_USER, 1);
 }
 
-void invokeDevKitSensors(void)
+void turnOffUserLED(void)
 {
-    if (blinkTime < 0)
+    digitalWrite(LED_USER, 0);
+}
+
+void turnOnRGBLED(int red, int green, int blue)
+{
+    rgbLed.setColor(red, green, blue);
+}
+
+void turnOffRGBLED(void)
+{
+    rgbLed.turnOff();
+}
+
+void startBlinkDevKitUserLED(int msDuration)
+{
+    blinkUserLEDTimeStart = SystemTickCounterRead();
+    blinkUserLEDTime = (msDuration == -1 ? 0x7fffffffffffffff : msDuration);
+    userLEDStat = 1;
+    digitalWrite(LED_USER, userLEDStat);
+}
+
+void startBlinkDevKitRGBLED(int msDuration)
+{
+    blinkRGBLEDTimeStart = SystemTickCounterRead();
+    blinkRGBLEDTime = (msDuration == -1 ? 0x7fffffffffffffff : msDuration);
+    colorRGBLED = 0;
+    rgbLed.setColor(_rgb[colorRGBLED].red, _rgb[colorRGBLED].green, _rgb[colorRGBLED].blue);
+}
+
+int textOutDevKitScreen(unsigned int line, const char *s, int wrap)
+{
+    Screen.print(line, s, (bool)wrap);
+}
+
+void drawDevKitScreen(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, unsigned char *image)
+{
+    Screen.draw(x0, y0, x1, y1, image);
+}
+
+void cleanDevKitScreen(void)
+{
+    Screen.clean();
+}
+
+int getButtonAState(void)
+{
+    return (digitalRead(USER_BUTTON_A) == LOW ? 1 : 0);
+}
+
+int getButtonBState(void)
+{
+    return (digitalRead(USER_BUTTON_B) == LOW ? 1 : 0);
+}
+
+int transmitIrDA(unsigned char *data, int size, int timeout)
+{
+    return IrdaSensor->IRDATransmit(data, size, timeout);
+}
+
+static void _blinkRGBLED(uint64_t msNow)
+{
+    if (blinkRGBLEDTime <= 0)
     {
         return;
     }
 
-    uint64_t ms = SystemTickCounterRead() - blinkTimeStart;
+    uint64_t ms = msNow - blinkRGBLEDTimeStart;
     if (ms >= 500)
     {
-        ledStat = !ledStat;
-        digitalWrite(LED_USER, ledStat);
-
-        rgbLed.setColor(_rgb[ledColor].red, _rgb[ledColor].green, _rgb[ledColor].blue);
-        ledColor = (ledColor + 1) % (sizeof(_rgb) / sizeof(struct _tagRGB));
-
-        blinkTime -= ms;
-        blinkTimeStart = SystemTickCounterRead();
-        if (blinkTime == 0)
+        // Change the color
+        colorRGBLED = (colorRGBLED + 1) % (sizeof(_rgb) / sizeof(struct _tagRGB));
+        rgbLed.setColor(_rgb[colorRGBLED].red, _rgb[colorRGBLED].green, _rgb[colorRGBLED].blue);
+        
+        blinkRGBLEDTime -= ms;
+        if (blinkRGBLEDTime <= 0)
         {
-            // Disable
-            blinkTime = -1;
-        }
-        if (blinkTime < 0)
-        {
-            digitalWrite(LED_USER, 0);
+            // End
             rgbLed.turnOff();
         }
+        else
+        {
+            blinkRGBLEDTimeStart = msNow;
+        }
     }
+}
+
+static void _blinkUserLED(uint64_t msNow)
+{
+    if (blinkUserLEDTime < 0)
+    {
+        return;
+    }
+
+    uint64_t ms = msNow - blinkUserLEDTimeStart;
+    if (ms >= 500)
+    {
+        // Flip
+        userLEDStat = !userLEDStat;
+        digitalWrite(LED_USER, userLEDStat);
+
+        blinkUserLEDTime -= ms;
+        if (blinkRGBLEDTime <= 0)
+        {
+            // End
+            digitalWrite(LED_USER, 0);
+        }
+        else
+        {
+            blinkUserLEDTimeStart = msNow;
+        }
+    }
+}
+
+void invokeDevKitPeripheral(void)
+{
+    uint64_t msNow = SystemTickCounterRead();
+
+    _blinkRGBLED(msNow);
+
+    _blinkUserLED(msNow);
 }
